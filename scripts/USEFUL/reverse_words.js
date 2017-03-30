15192c6dcad3b25fbc2a86edb4c8e309beffd516
Matt D Harnetiaux <mattdharnetiaux@Matts-MacBook-Pro.local>
2017-03-30 11:50:30 -0700

    reverse

--- scripts/USEFUL/reverse_words.js
+++ scripts/USEFUL/reverse_words.js
@@ -1,16 +1,8 @@
-function reverseWords(str){
-    let rev = [],
-        wordLen = 0;
-    for(let i = str.length-1; i>=0; i--){
-        if(str[i]==' ' || i==0){
-            rev.push(str.substr(i,wordLen+1));
-            wordLen = 0;
-        }
-        else
-            wordLen++;
-    }
-    return rev.join(' ');
+function reverse(s) {
+    let o = '';
+    for (let i = s.length - 1; i >= 0; i--)
+        o += s[i];
+    return o;
 }
 
-
-console.log(reverseWords("matt"));
+console.log(reverse("hey there"));
