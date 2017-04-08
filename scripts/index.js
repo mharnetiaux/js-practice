fce60a57965b6a20659847e9f228c8dd2b7c09a6
Matt D Harnetiaux <mattdharnetiaux@Matts-MacBook-Pro.local>
2017-04-08 01:18:39 -0700

    more nav fun

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,56 @@
-let click_icon = document.querySelector("#click_icon"),
+const click_icon = document.querySelector("#click_icon"),
     nav_element = document.querySelector('#nav_header'),
-    ul_element = document.querySelector("#menu");
+    ul_element = document.querySelector("#menu"),
+    open = "nav_open",
+    closed = "",
+    close_nav_img = "close_nav_img";
 
 click_icon.addEventListener("click", function(event) {
     switch(ul_element.className){
-        case "open":
-            click_icon.className = "";
-            ul_element.className = "";
-            console.log("closed");
+        case open:
+            click_icon.className = closed;
+            ul_element.className = closed;
             break;
         case "":
-            click_icon.className = "open_click";
-            ul_element.className = "open";
-            console.log("open");
+            click_icon.className = close_nav_img;
+            ul_element.className = open;
             break;
         default:
-            click_icon.className = "";
-            ul_element.className = "";
-            console.log("closed");
+            click_icon.className = closed;
+            ul_element.className = closed;
     }
     event.preventDefault();
 }, false);
 
 document.addEventListener('click', (event) => {
     let isClickInside = nav_element.contains(event.target);
-    if (!isClickInside && ul_element.className === "open") {
+    if (!isClickInside && ul_element.className === open) {
         ul_element.className = "";
         click_icon.className = "";
     }
 });
+
+/*class navMenu{
+    constructor(click_icon, nav_element, ul_element){
+        this.click_icon = click_icon;
+        this.nav_element = nav_element;
+        this.ul_element = ul_element;
+
+        click_icon.addEventListener("click", function(event){
+            switch(ul_element.className){
+                case "open":
+                    click_icon.className = "";
+                    ul_element.className = "";
+                    break;
+                case "":
+                    click_icon.className = "open_click";
+                    ul_element.className = "open";
+                    break;
+                default:
+                    click_icon.className = "";
+                    ul_element.className = "";
+            }
+            event.preventDefault();
+        },false);
+    }
+}*/
